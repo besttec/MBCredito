1502bb673715d5dd67c294b87122409409f48c11
Fabio Aguiar <fabinhobarreto2@gmail.com>
2014-12-16 11:35:35 -0300

    16/12/2014

--- src/SerBinario/MBCredito/MBCreditoBundle/Controller/DefaultController.php
+++ src/SerBinario/MBCredito/MBCreditoBundle/Controller/DefaultController.php
@@ -5,6 +5,9 @@ namespace SerBinario\MBCredito\MBCreditoBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 
 class DefaultController extends Controller
 {
@@ -15,5 +18,44 @@ class DefaultController extends Controller
     public function indexAction()
     {
         return array();
+    }
+    
+    /**
+     * @Route("/importarArquivo")
+     * @Template()
+     */
+    public function viewImportarArquivoAction()
+    {
+        return array();
+    }
+    
+    /**
+     * @Route("/inserirDados")
+     * @Template()
+     */
+    public function viewInserirDadosAction()
+    {
+        return array();
+    }
+    
+    /**
+     * @Route("/inserirDados")
+     * @Method({"POST"})
+     * @Template("")
+     */
+    public function testeGridAction()
+    {
+        $Array = array();
+        $Array[1]['DT_RowId']   =  1;
+        $Array[1]["casa"] = "casa";
+        $Array[1]["Carro"] = "Carro";
+        $Array[1]["onibus"] = "Ônibus";
+        
+        $columns = array(               
+                
+                'data' => $Array               
+            );
+        
+        return new JsonResponse($columns);
     }
 }
