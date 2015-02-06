f28dbaaec7dfc1fbaa58e22d30a548c444813d8c
Andrey <andreyfbs2013@gmail.com>
2015-02-06 10:21:10 -0300

    Atualizações

--- src/SerBinario/MBCredito/MBCreditoBundle/Controller/DefaultController.php
+++ src/SerBinario/MBCredito/MBCreditoBundle/Controller/DefaultController.php
@@ -909,7 +909,7 @@ class DefaultController extends Controller
         
         $userVal = $validator->validate($user);
         
-        if(count($userVal) > 0) {
+        if( !count($userVal) > 0) {
             $userDAO = new UserDAO($this->getDoctrine()->getManager());
             $result  = $userDAO->save($user);
 
